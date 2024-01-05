c02c187f6d4af5986bc2d55e2e190c58b9df73be
Adhham Safwan <msmadhhamg@gmail.com>
2024-01-05 19:20:32 +0000

    Add auth and design

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/shared/Header'
+import Footer from '@/components/shared/Footer'
+
+export default function Layout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <div className='flex h-screen flex-col'>
+        <Header />
+        <main className='flex-1'>{children}</main>
+        <Footer />
+      </div>
+    )
+  }
+  
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <main>
+    <h1 className='text-4xl'></h1>
+   </main>
+  )
+}
